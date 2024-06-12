66f5a811a75e2dc5ee173388971ac2146e583f5e
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-06-11 21:53:28 -0300

    finalizando o exercicio.

---
+++ my-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Leão</h1>
+
+        <p> O leão é conciderado o rei da Selva.</p>
+        <button onClick={() => alert('Saiba mais sobre os Leões!')}>Saiba Mais</button>
+      </div>
+    </>
+  )
+}
+
+export default App
